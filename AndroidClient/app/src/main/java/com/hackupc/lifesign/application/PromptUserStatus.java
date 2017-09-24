513ec4bcd9140143b7eb2e3041fb671f1166d504
Toni Väisänen <vaisanen.toni@gmail.com>
2017-09-24 18:48:08 +0200

    If app is in the background, Notification is used instead of AlertDialog. Controls need to implemented (y/n).

--- AndroidClient/app/src/main/java/com/hackupc/lifesign/application/PromptUserStatus.java
+++ AndroidClient/app/src/main/java/com/hackupc/lifesign/application/PromptUserStatus.java
@@ -1,11 +1,17 @@
 package com.hackupc.lifesign.application;
 
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 
+import com.hackupc.lifesign.R;
 import com.hackupc.lifesign.activities.MainActivity;
 
 /**
@@ -13,29 +19,51 @@ import com.hackupc.lifesign.activities.MainActivity;
  */
 
 public class PromptUserStatus extends BroadcastReceiver {
+
+    private static final String TAG = "app.PromptUserStatus";
+
+
     @Override
     public void onReceive(Context context, Intent intent){
 
-        System.out.println("hello from prompt!");
         context = MainActivity.appContext;
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
 
-        alertBuilder.setTitle("Hello Chris!");
-        alertBuilder.setMessage("Are you dead yet?");
+        if (Lifesign.isActivityVisible()){
+            Log.v(TAG, "Activity visible -> Use AlertDialog");
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
 
-        alertBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int which){
-                dialog.dismiss();
-            }
-        });
+            alertBuilder.setTitle("Hello Chris!");
+            alertBuilder.setMessage("Are you dead yet?");
 
-        alertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int which){
-                dialog.dismiss();
-            }
-        });
+            alertBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.dismiss();
+                }
+            });
 
-        AlertDialog alert = alertBuilder.create();
-        alert.show();
+            alertBuilder.setNegativeButton("No", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.dismiss();
+                }
+            });
+
+            AlertDialog alert = alertBuilder.create();
+            alert.show();
+        } else {
+            Log.v(TAG, "Activity background -> Use Notification");
+
+            NotificationManager nm = (NotificationManager) context
+                    .getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context);
+
+            notificationBuilder.setContentTitle("Chris, are you dead yet?");
+            notificationBuilder.setSmallIcon(R.drawable.ic_menu_send);
+            notificationBuilder.setPriority(Notification.PRIORITY_MAX);
+            notificationBuilder.setDefaults(Notification.DEFAULT_ALL);
+
+            Notification notification = notificationBuilder.build();
+            nm.notify(0, notification);
+        }
+
     }
 }
